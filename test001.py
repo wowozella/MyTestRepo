1cce67c94407e2b29e67590584d622dff6a28d43
Jalil_Kowsarian <wowozella@gmail.com>
2025-06-10 16:24:42 +0330

    My first file created and trying to use git and github for learning...

---
+++ test001.py
@@ -0,0 +1,12 @@
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+print('Hello Wowozella')
+num = int(input('Please enter a Number:'))
+print(num)
+print("----"*5)
+
+class MyClass:
+    ...
